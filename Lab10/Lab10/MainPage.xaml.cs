5575c5ab98e65eaad9af3fe760a494f86e216b11
cuellar2020 <67763251+cuellar2020@users.noreply.github.com>
2022-05-25 13:42:20 -0400

    Add project files.

---
+++ Lab10/Lab10/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            item01.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new ViewToViewDemo());
+            };
+
+            item02.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new BindingModeDemo());
+            };
+
+            item03.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new ListViewDemo());
+            };
+
+            item04.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new PickerDemo());
+            };
+        }
+    }
+}
